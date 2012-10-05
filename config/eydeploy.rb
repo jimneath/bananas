040edad3f582f43fcb659c707953f95c7f11473b
Jim Neath <jimneath@googlemail.com>
2012-10-05 11:05:40 +0100

    bananas

--- config/eydeploy.rb
+++ config/eydeploy.rb
@@ -1,10 +1,10 @@
 def compile_assets
   roles :app_master, :app, :solo, :util do
     shell.status "Precompiling assets for rails v#{bundled_rails_version}"
-    cmd = "cd #{paths.active_release} && PATH=#{paths.binstubs}:$PATH #{config.framework_envs} rake assets:precompile"
+    run("mkdir -p #{paths.shared}/assets && ln -sn #{paths.shared}/assets #{paths.public_assets}") 
 
     begin
-      run(cmd)
+      run("cd #{paths.active_release} && PATH=#{paths.binstubs}:$PATH #{config.framework_envs} rake assets:precompile")
     rescue StandardError => e
       if config.precompile_assets_inferred?
         shell.notice "Asset compilation failure ignored.\nAdd 'precompile_assets: true' to ey.yml to abort deploy on failure."
