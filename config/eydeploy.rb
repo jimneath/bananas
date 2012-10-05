252aed40e9dc504067bab5ec6316589503ad6e39
Jim Neath <jimneath@googlemail.com>
2012-10-05 10:08:04 +0100

    bananas

--- config/eydeploy.rb
+++ config/eydeploy.rb
@@ -1,24 +1,12 @@
 def compile_assets
-  shell.status "bananas"
-  rails_version = bundled_rails_version
-  
   roles :app_master, :app, :solo, :util do
+    shell.status "Precompiling assets for rails v#{bundled_rails_version}"
     cmd = "cd #{paths.active_release} && PATH=#{paths.binstubs}:$PATH #{config.framework_envs} rake assets:precompile"
-
-    if rails_version
-      shell.status "Precompiling assets for rails v#{rails_version}"
-    else
-      shell.warning "Precompiling assets even though Rails was not bundled."
-    end
 
     begin
       run(cmd)
     rescue StandardError => e
       if config.precompile_assets_inferred?
-        # If specifically requested, then we want to fail if compilation fails.
-        # If we are implicitly precompiling, we want to fail non-destructively
-        # because we don't know if the rake task exists or if the user
-        # actually intended for assets to be compiled.
         shell.notice "Asset compilation failure ignored.\nAdd 'precompile_assets: true' to ey.yml to abort deploy on failure."
         return
       else
